6e2515c8c9c0af1003a3107538c0d4172d43f589
Sebas <sbriones284@gmail.com>
2023-02-06 20:55:19 -0600

    Initial commit

---
+++ src/main/java/com/TiendaTenis/TiendaTenisApplication.java
@@ -0,0 +1,13 @@
+package com.TiendaTenis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaTenisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaTenisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TiendaTenis/TiendaTenisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TiendaTenis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaTenisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
